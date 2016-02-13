03f5758a946dc6df21e92f889efddf141d7803a2
konawwww <konawwww@gmail.com>
2016-02-14 02:49:50 +1100

    Add a pretty dumb method to guess which ip the server started on
    I'm sure there's a better way

--- multiplayer/network_server.rb
+++ multiplayer/network_server.rb
@@ -5,6 +5,15 @@ class NetworkServer
     @port = port
   end
 
+  # Probable v4 ip address
+  def ip_address
+    Socket.ip_address_list.reject { |addr_info|
+      addr_info.ipv6?
+    }.reject { |addr_info|
+      addr_info.ip_address == "127.0.0.1"
+    }.first.ip_address
+  end
+
   def start
     raise "Server already started!" if @server_pid
 
--- spec/multiplayer/network_server_spec.rb
+++ spec/multiplayer/network_server_spec.rb
@@ -23,4 +23,19 @@ RSpec.describe NetworkServer do
       server.start
     end
   end
+
+  describe "#ip" do
+    before do
+      allow(Socket).to receive(:ip_address_list).and_return [
+        Addrinfo.new(Socket.sockaddr_in(80, "127.0.0.1")),
+        Addrinfo.new(Socket.sockaddr_in(80, "2001:DB8::1")),
+        Addrinfo.new(Socket.sockaddr_in(80, "10.1.2.3"))
+      ]
+    end
+
+    it "returns the first ip that isn't v6 or localhost" do
+      server = NetworkServer.new
+      expect(server.ip_address).to eq "10.1.2.3"
+    end
+  end
 end
